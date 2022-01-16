0e713f31b9544f201f7d84545d10a810543cddd1
Lauren Herman <thisisjustfabulous.0@gmail.com>
2022-01-15 18:57:18 -0700

    Move Working

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 var currentPos
+onready var player = get_node("/root/Combat/Player")
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		print("Clicked")
 		print(self.position)
 		currentPos = self.position
+		player.position.x = self.position.x
+		player.position.y = self.position.y
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
