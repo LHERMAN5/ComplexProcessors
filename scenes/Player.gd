865ad3853bdd9308d8ebf9a80ba7bb105d10a104
Lauren Herman <thisisjustfabulous.0@gmail.com>
2022-01-15 17:53:28 -0700

    Areas Added for Tiles

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,19 +1,20 @@
-extends Node2D
+extends Sprite
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-var currentTile = 7
+var area2d = preload("res://scripts/Area2D.gd")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("move to:")
-	print(currentTile)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	print(area2d.currentPos)
+	#position = area.currentPos
 #	pass
--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,19 +1,12 @@
 extends Area2D
 
-
-var combat = preload("res://scripts/Combat.gd")
-
-onready var player = get_node("/root/Combat/Player")
+var currentPos
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		print("Clicked")
-		print(player.position)
 		print(self.position)
-		player.position == Vector2(0,0)
-		print(player.position)
-		print(self.position)
-		#combat.currentTile=1
+		currentPos = self.position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
